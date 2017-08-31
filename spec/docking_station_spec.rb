9555036096241b6595ad43a974382fc67e13abfc
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-08-31 16:48:05 +0100

    Updated tests and docking_station.rb with bike instance variable

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,23 @@
 require 'docking_station'
 
 describe DockingStation do
+  subject(:docking_station) {described_class.new}
+
   it {is_expected.to respond_to :release_bike}
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = docking_station.release_bike
     expect(bike).to be_working
+  end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
