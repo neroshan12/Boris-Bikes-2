1f95388102016605a9678e37c83ff75717a61eb9
Nero Siva <neroshan12@gmail.com>
2017-09-01 16:44:37 +0100

    Add private methods (Step 15)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,16 +7,26 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
   end
 
   def bike
-    @bike = Bike.new 
+    @bike = Bike.new
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
